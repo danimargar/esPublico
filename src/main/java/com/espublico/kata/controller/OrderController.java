f369cd4e3e95db8fd8892b88348e8ce6bd33504c
Daniel Martín Gargallo <danimargar@gmail.com>
2023-07-19 12:05:40 +0200

    Primer commit

--- src/main/java/com/espublico/kata/controller/OrderController.java
+++ src/main/java/com/espublico/kata/controller/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("listOrders")
 public class OrderController {
 
+
     @Value("${kata.page}")
     protected String page;
 
